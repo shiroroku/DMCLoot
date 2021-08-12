2d0135c1335040d9299eec32d28b31836775b3be
Elise <elseytd@gmail.com>
2021-08-12 15:48:27 -0700

    added essence tooltip

--- src/main/java/com/dmcloot/Item/EssenceItem.java
+++ src/main/java/com/dmcloot/Item/EssenceItem.java
@@ -1,11 +1,19 @@
 package com.dmcloot.Item;
 
 import com.dmcloot.Modifier.IModifier;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class EssenceItem extends Item {
 
@@ -18,6 +26,11 @@ public class EssenceItem extends Item {
 
 	public IModifier.Rarity getRarity() {
 		return rarity;
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public void appendHoverText(ItemStack stack, @Nullable World world, List<ITextComponent> text, ITooltipFlag flag) {
+		text.add(new TranslationTextComponent("item.dmcloot.essence.desc", new TranslationTextComponent("rarity.dmcloot." + getRarity().toString())).setStyle(Style.EMPTY.withColor(TextFormatting.GRAY)));
 	}
 
 	@Override
